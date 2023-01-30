8ebc9d760d2bfde4d2a023005ffa86f2e24af95e
Berishten <vepaes439@gmail.com>
2023-01-29 22:59:12 -0300

    add location control

--- js/MapBoxModule.js
+++ js/MapBoxModule.js
@@ -10,6 +10,20 @@ export default class Map {
       zoom: 16,
     });
 
+    this.map.on("load", () => {
+      this.map.addControl(
+        new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true,
+          },
+          // When active the map will receive updates to the device's location as it changes.
+          trackUserLocation: true,
+          // Draw an arrow next to the location dot to indicate which direction the device is heading.
+          showUserHeading: true,
+        })
+      );
+    });
+
     // Create events
     this.map.on("load", () => {
       let mapp = this.map;
